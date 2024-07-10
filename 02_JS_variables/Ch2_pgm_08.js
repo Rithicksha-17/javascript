844aa4b2310d5421091b869486cb2a9aae747999
Rithicksha <rithicksha1742003@gmail.com>
2024-07-10 15:10:54 +0530

    completed

--- 02_JS_variables/Ch2_pgm_08.js
+++ 02_JS_variables/Ch2_pgm_08.js
@@ -3,12 +3,14 @@
 var score;
 score = 100;
 console.log("Your score was " + score);
-
 console.log("Great splat!!!");
 score = score + 50;
 console.log("New score: " + score);
-
 console.log("Way to go!");
+console.log("You splatted a kumquat!");
+score= score + 100; 
+console.log("New Score:" + score);
+console.log("Congratulations!!");
 
 
 
