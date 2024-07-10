acc165f0158c1dd0f178aa743ebad75c384ed24c
Rithicksha <rithicksha1742003@gmail.com>
2024-07-10 14:56:59 +0530

    completed

--- 02_JS_variables/Ch2_pgm_06.js
+++ 02_JS_variables/Ch2_pgm_06.js
@@ -2,11 +2,13 @@
 
 var playerName;
 var locationName;
-
+var score;
 playerName = "Lionel Messi";
 locationName = "Argentina";
-
+score = "200";
 console.log(playerName + " is in " + locationName);
+console.log(playerName + "has a score of" + score);
+
 
 
 
