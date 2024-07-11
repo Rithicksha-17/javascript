36fd547105e2054deedec39695f63d914623f315
Rithicksha <rithicksha1742003@gmail.com>
2024-07-11 14:42:34 +0530

    completed

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -8,6 +8,23 @@ var post = {
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
 
+console.log("Post ID: " + post.id);
+console.log("Title: " + post.title);
+console.log("Author: " + post.author);
+console.log("Created on: " + post.created);
+
+var post2 = {
+    id: 2,
+    title: "think straight",
+    author: "Philae",
+    created: "2000-03-14",
+    body: "After a long journey, I'm finally back home!"
+};
+
+console.log("Post ID: " + post2.id);
+console.log("Title: " + post2.title);
+console.log("Author: " + post2.author);
+console.log("Created on: " + post2.created);
 
 
 /* Further Adventures
