7e28c456f0e4e1900b9ce2cf7dbec2ecbd6e4d4f
Rithicksha <rithicksha1742003@gmail.com>
2024-07-11 15:33:15 +0530

    completed

--- 04_JS_Functions/Ch4_pgm_05.js
+++ 04_JS_Functions/Ch4_pgm_05.js
@@ -7,12 +7,28 @@ findTotal = function () {
 	result = number1 + number2;
 };
 
+var number1 = 5;
+var number2 = 10;
+var result;
+
+var findTotal = function () {
+    result = number1 + number2;
+};
+
 displayMenu = function () {
 	console.log("Please choose an option:");
 	console.log("(1) Print log");
 	console.log("(2) Upload file");
 	console.log("(9) Quit");
 };
+
+findTotal();
+
+
+console.log("Result of number1 + number2 = " + result);
+
+
+displayMenu();
 
 
 
