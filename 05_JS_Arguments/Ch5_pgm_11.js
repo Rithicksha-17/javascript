057e28a01128ecb5bb98baf1327f1d8dbdc7547d
Rithicksha <rithicksha1742003@gmail.com>
2024-07-12 14:53:38 +0530

    completed

--- 05_JS_Arguments/Ch5_pgm_11.js
+++ 05_JS_Arguments/Ch5_pgm_11.js
@@ -10,6 +10,26 @@ showPlayerPlace("Kandra", "The Dungeon of Doom");
 showPlayerPlace("Dax", "The Old Library");
 
 
+var showPlayerPlace;
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName[1] + " is in " + playerPlace);
+};
+
+showPlayerPlace("Kandra", "The Dungeon of Doom");
+showPlayerPlace("Dax", "The Old Library");
+
+var showPlayerPlace;
+
+showPlayerPlace = function (playerName, playerPlace) {
+    console.log(playerName[3] + " is in " + playerPlace);
+};
+
+showPlayerPlace("Kandra", "The Dungeon of Doom");
+showPlayerPlace("Dax", "The Old Library");
+
+
+
 
 /* Further Adventures
  *
