bc664b636f2d2fcef844a708ddf03111efcb0df6
Rithicksha <rithicksha1742003@gmail.com>
2024-07-12 14:28:34 +0530

    completed

--- 05_JS_Arguments/Ch5_pgm_04.js
+++ 05_JS_Arguments/Ch5_pgm_04.js
@@ -3,12 +3,22 @@
 var showMessage;
 
 showMessage = function (message) {
-	console.log("The message is: " + message);
+    console.log("The message is:");
+    console.log(message);
 };
 
 showMessage("It's full of stars!");
 showMessage("Hello to Jason Isaacs");
 showMessage("Hello to Jason Isaacs and Stephen Fry");
+
+
+
+
+var myMessage = "This is my custom message.";
+
+
+showMessage(myMessage);
+
 
 
 
