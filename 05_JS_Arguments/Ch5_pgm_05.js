6710836ac969d988d074b1e1b694e97a1f407604
Rithicksha <rithicksha1742003@gmail.com>
2024-07-12 14:30:31 +0530

    completed

--- 05_JS_Arguments/Ch5_pgm_05.js
+++ 05_JS_Arguments/Ch5_pgm_05.js
@@ -13,8 +13,29 @@ square(-2);
 square(1111);
 square(0.5);
 
+var cube = function (numberToCube) {
+  var result;
+  result = numberToCube * numberToCube * numberToCube;
+  console.log(numberToCube + " * " + numberToCube + " * " + numberToCube + " = " + result);
+};
+
+cube(2);
+cube(-3);
+cube(0);
+cube(1.5);
 
 
+var squareRoot = function (number) {
+  var result;
+  result = Math.sqrt(number);
+  console.log("The square root of " + number + " is " + result);
+};
+
+// Test the squareRoot function
+squareRoot(9);
+squareRoot(16);
+squareRoot(2);
+squareRoot(0.25);
 /* Further Adventures
  *
  * 1) Define a cube function that cubes
