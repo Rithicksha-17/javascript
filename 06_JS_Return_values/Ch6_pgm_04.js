1890dd5eff7b4475b63f7300e972c56d2e0e3ee3
Rithicksha <rithicksha1742003@gmail.com>
2024-07-14 21:53:15 +0530

    completed

--- 06_JS_Return_values/Ch6_pgm_04.js
+++ 06_JS_Return_values/Ch6_pgm_04.js
@@ -12,6 +12,26 @@ var sum = add(50, 23);
 
 console.log(sum);
 
+var sum1 = add(10, 15);
+console.log(sum1); 
+
+
+var number1 = 50;
+var number2 = 23;
+var sum2 = add(number1, number2);
+console.log('The sum of ' + number1 + ' and ' + number2 + ' is ' + sum2);
+
+var sum3 = add(add(5, 10), 20);
+console.log(sum3);
+
+
+var addThreeNumbers = function (number1, number2, number3) {
+    return number1 + number2 + number3;
+};
+
+var sum4 = addThreeNumbers(5, 10, 15);
+console.log(sum4);
+
 
 
 /* Further Adventures
