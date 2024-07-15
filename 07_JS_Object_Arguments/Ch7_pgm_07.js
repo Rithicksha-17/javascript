a977af113f29b4fc281be6251d1b8d606bf8929e
Rithicksha <rithicksha1742003@gmail.com>
2024-07-15 14:33:06 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_07.js
+++ 07_JS_Object_Arguments/Ch7_pgm_07.js
@@ -5,6 +5,27 @@ var bigPlanet = planet.toUpperCase();
 
 console.log(planet + " becomes " + bigPlanet);
 
+// Function to convert a string to upper case
+var getBig = function (str) {
+    return str.toUpperCase();
+};
+
+// Function to convert a string to lower case
+var getSmall = function (str) {
+    return str.toLowerCase();
+};
+
+// Test the functions
+var testString1 = "Earth";
+var testString2 = "NePtUnE";
+
+console.log(testString1 + " becomes " + getBig(testString1));
+console.log(testString2 + " becomes " + getBig(testString2));
+
+console.log(testString1 + " becomes " + getSmall(testString1));
+console.log(testString2 + " becomes " + getSmall(testString2));
+
+
 
 
 /* Further Adventures
