1bf39b0e4e7d224312a1c2269d0dd23b68d88431
Rithicksha <rithicksha1742003@gmail.com>
2024-07-15 14:28:02 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_05.js
+++ 07_JS_Object_Arguments/Ch7_pgm_05.js
@@ -7,10 +7,18 @@ var showSmaller = function (num1, num2) {
     console.log(smaller + " is smaller than " + larger);
 };
 
+var showLarger = function (num1, num2) {
+    var larger = Math.max(num1, num2);
+    var smaller = Math.min(num1, num2);
+
+    console.log(larger + " is larger than " + smaller);
+};
+
 showSmaller(12, 3);
 showSmaller(-10, 3);
 
-
+showLarger(12, 3);
+showLarger(-10, 3);
 
 /* Further Adventures
  *
