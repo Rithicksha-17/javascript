96fe8ef7936e5bbc2aa2c8e4c02916ed4fd067da
Rithicksha <rithicksha1742003@gmail.com>
2024-07-15 14:37:38 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_08.js
+++ 07_JS_Object_Arguments/Ch7_pgm_08.js
@@ -3,7 +3,37 @@
 var message = "We choose to go to the Moon!";
 
 console.log(message.substr(3, 12));
+console.log(message.substr(21, 5));
+console.log(message.substr(11, 9));
+console.log(message.substr(0, 9))
 
+console.log(message.substr(3)); 
+
+
+console.log(message.substr(-5)); 
+
+console.log(message.substr(3, -2)); 
+
+
+console.log(message.substr(-10, 5)); 
+
+var message = "We choose to go to the Moon!";
+
+console.log(message.substr(3, 12)); 
+
+console.log(message.substr(21, 5)); 
+
+
+console.log(message.substr(11, 9));
+
+console.log(message.substr(0, 9)); 
+
+
+console.log(message.substr(3)); 
+
+console.log(message.substr(-5)); 
+console.log(message.substr(3, -2));
+console.log(message.substr(-10, 5)); 
 
 
 /* Further Adventures
