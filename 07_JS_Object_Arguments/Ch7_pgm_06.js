3c4a76918a87ba5fa2fd2c12fc8e3aa9cdcade4b
Rithicksha <rithicksha1742003@gmail.com>
2024-07-15 14:31:29 +0530

    ccompleted

--- 07_JS_Object_Arguments/Ch7_pgm_06.js
+++ 07_JS_Object_Arguments/Ch7_pgm_06.js
@@ -1,15 +1,44 @@
 // Using Math.min and Math.max to constrain an argument
-
 var line = function (lineLength) {
-    var line = "========================================";
-    lineLength = Math.max(0, lineLength);
-    lineLength = Math.min(40, lineLength);
-    return line.substr(0, lineLength);
-  };
-  
-  console.log(line(30));
-  console.log(line(40));
-  console.log(line(50));
+  var line = "========================================";
+  lineLength = Math.max(0, lineLength);
+  lineLength = Math.min(40, lineLength);
+  return line.substr(0, lineLength);
+};
+
+var spaces = function (spaceLength) {
+  var space = "                                        "; // 40 spaces
+  spaceLength = Math.max(0, spaceLength);
+  spaceLength = Math.min(40, spaceLength);
+  return space.substr(0, spaceLength);
+};
+
+var emptyBox = function (width) {
+  width = Math.max(1, Math.min(40, width)); // Ensure width is between 1 and 40
+  var topBottom = line(width);
+  var middle = "=" + spaces(width - 2) + "=";
+  console.log(topBottom);
+  for (var i = 0; i < 3; i++) {
+      console.log(middle);
+  }
+  console.log(topBottom);
+};
+
+// Test line lengths from -20 to 60 in steps of 10
+for (var i = -20; i <= 60; i += 10) {
+  console.log("line(" + i + "): " + line(i));
+}
+
+// Test spaces function
+console.log("spaces(10): '" + spaces(10) + "'");
+console.log("spaces(40): '" + spaces(40) + "'");
+console.log("spaces(50): '" + spaces(50) + "'");
+
+// Test emptyBox function
+emptyBox(12);
+emptyBox(5);
+emptyBox(40);
+
   
   
   
