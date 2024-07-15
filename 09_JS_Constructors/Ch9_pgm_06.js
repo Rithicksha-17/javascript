2c2cc5cea9e2fc756e98acb54490cda6f8be5a4b
Rithicksha <rithicksha1742003@gmail.com>
2024-07-15 19:50:05 +0530

    completed

--- 09_JS_Constructors/Ch9_pgm_06.js
+++ 09_JS_Constructors/Ch9_pgm_06.js
@@ -3,15 +3,34 @@ var Planet = function (name, position, type) {
     this.position = position;
     this.type = type;
     this.moons = [];
-  
+
     this.showPlanet = function () {
         console.log(this.name);
         console.log("Planet " + this.position + " - " + this.type);
-        console.log("Moons: " + this.moons.join(', ') + ".");
+        this.showMoons();
     };
-  
+
     this.addMoon = function (moon) {
         this.moons.unshift(moon);
+    };
+
+    this.showMoons = function () {
+        if (this.moons.length > 0) {
+            console.log("Moons:");
+            this.moons.forEach(function (moon, index) {
+                console.log("(" + index + ") " + moon);
+            });
+        } else {
+            console.log("Moons: None.");
+        }
+    };
+
+    this.getMoon = function (index) {
+        if (index >= 0 && index < this.moons.length) {
+            return this.moons[index];
+        } else {
+            return "Invalid index.";
+        }
     };
 };
 
@@ -19,14 +38,28 @@ var planet1 = new Planet("Jupiter", 5, "Gas Giant");
 planet1.addMoon("Io");
 planet1.addMoon("Europa");
 
+// Add a new moon to planet1
+planet1.addMoon("Ganymede");
+
 var planet2 = new Planet("Neptune", 8, "Ice Giant");
 planet2.addMoon("Triton");
 
+// Add a new moon to planet2
+planet2.addMoon("Nereid");
+
 var planet3 = new Planet("Mercury", 1, "Terrestrial");
+
+// Add a new moon to planet3 (even though Mercury has no moons, for the sake of the example)
+planet3.addMoon("Hypothetical");
 
 [ planet1, planet2, planet3 ].forEach(function (planet) {
     planet.showPlanet();
 });
+
+// Test getMoon method
+console.log(planet1.getMoon(1)); // Output: Europa
+console.log(planet2.getMoon(0)); // Output: Nereid
+console.log(planet3.getMoon(2)); // Output: Invalid index.
 
 
 
