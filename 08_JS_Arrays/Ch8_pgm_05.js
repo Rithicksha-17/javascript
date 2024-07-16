ca8a8b4aea92fe78d36bc58f696918b30f46f360
Rithicksha <rithicksha1742003@gmail.com>
2024-07-16 20:37:22 +0530

    completed

--- 08_JS_Arrays/Ch8_pgm_05.js
+++ 08_JS_Arrays/Ch8_pgm_05.js
@@ -1,29 +1,57 @@
 // Passing an array to a function
 
-var getVisitorReport = function (visitorArray, dayInWeek) {
-	var days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday"
-  ];
-	var index = dayInWeek - 1;
-	var visitorReport;
 
-	visitorReport = "There were ";
+var getVisitorReport = function (visitorArray, dayInWeek) {
+  var days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+  ];
+  var index = dayInWeek - 1;
+  var visitorReport;
+
+  visitorReport = "There were ";
   visitorReport += visitorArray[index];
   visitorReport += " visitors ";
-	visitorReport += "on " + days[index];
+  visitorReport += "on " + days[index];
 
-	return visitorReport;
+  return visitorReport;
 };
 
-var visitors = [ 354, 132, 210, 221, 481 ];
+var visitors = [354, 132, 210, 221, 481, 99, 150];
 
-var report = getVisitorReport(visitors, 2);
 
-console.log(report);
+var report = getVisitorReport(visitors, 5);
+console.log(report); 
+
+
+var getMonthlyVisitorReport = function(monthArray, week, dayInWeek) {
+  var weekArray = monthArray[week - 1];
+  return getVisitorReport(weekArray, dayInWeek);
+};
+
+
+var week1 = [100, 200, 150, 250, 300, 180, 220];
+var week2 = [110, 210, 160, 260, 310, 190, 230];
+var week3 = [120, 220, 170, 270, 320, 200, 240];
+var week4 = [130, 230, 180, 280, 330, 210, 250];
+
+var month = [week1, week2, week3, week4];
+
+
+var report1 = getMonthlyVisitorReport(month, 1, 1);
+console.log(report1); 
+
+var report2 = getMonthlyVisitorReport(month, 3, 4); 
+console.log(report2);
+
+var report3 = getMonthlyVisitorReport(month, 4, 7); 
+console.log(report3); 
+
 
 
 
